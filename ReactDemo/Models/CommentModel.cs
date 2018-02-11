b245157bc67f3fa295358b0993b651f63377b71b
tjcertified@gmail.com <uJCTU7ZXxYPNkc2XKHyJ4ZzyC>
2018-02-10 23:38:46 -0600

    Initial commit

---
+++ ReactDemo/Models/CommentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ReactDemo.Models
+{
+    public class CommentModel
+    {
+        public CommentModel() {}
+
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Text { get; set; }
+    }
+}
